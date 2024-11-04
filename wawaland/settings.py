2699204e4cf02f832c54f1b2dd34da56a153c093
Stephanie Bell <stephaniebellpalomino@hotmail.com>
2024-11-04 19:02:20 +0000

    Add secret key

--- wawaland/settings.py
+++ wawaland/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2)&5v-^=o!!e(w-(%dkt6cywaw23!2j5ac$gtd*3j(uli62w*^'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-bpstephanie-wawaland-e0ke2fxahyz.ws.codeinstitute-ide.net','.herokuapp.com']
 
