21df2a257384bdce3b3eccbb7fb5589f666933ca
Stephanie Bell <stephaniebellpalomino@hotmail.com>
2024-11-06 02:22:07 +0000

    Add content to blog, index, 404, and base.html. Add images. Alter urls.py to lead users to index.html first

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views.generic import TemplateView, ListView, View
 from .models import Post
 
 # Create your views here.
-class PostList(generic.ListView):
+class Home(TemplateView):
+    template_name = 'index.html'
+
+class PostList(ListView):
     queryset = Post.objects.filter(status=1)
-    template_name = "blog/index.html"
+    template_name = "blog.html"
     paginate_by = 6
+
