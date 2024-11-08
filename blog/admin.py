ba43edff0fb34942520560c85fafccb0c1a5d744
Stephanie Bell <stephaniebellpalomino@hotmail.com>
2024-11-08 21:59:33 +0000

    Add styling to login, logout and sign up pages

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
@@ -13,5 +13,5 @@ class PostAdmin(SummernoteModelAdmin):
 
 
 # Register your models here.
-#admin.site.register(Comment)
+admin.site.register(Comment)
 
